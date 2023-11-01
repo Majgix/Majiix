045a8cf9c185e4f537f355d7a81501b4216e40af
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-11-01 23:11:21 +0300

    strt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+//maajix is a web-based video streaming service. 
+//Technologies:
+
+// Webcodecs -> for video and audio encoding and decoding
+//WebTransport -> for transmitting the video and audio over a network
+// 
