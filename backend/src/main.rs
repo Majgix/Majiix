df7eee611dd245e213bb1ed151277d2ce083c930
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-11-05 22:38:12 +0300

    add axum, tokio

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, Server, response, Json, routing::get};
+
+async fn start_stream() -> response::Json<&'static str> {
+   Json("Yay!! WebTransport Connection established!")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/start-stream", get(start_stream));
+
+    let addr = SocketAddr::from(([127, 0 , 0, 1], 8080));
+    println!("server started at addr: http://{:?}", addr);
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+
+   
 }
 
 
@@ -7,5 +27,4 @@ fn main() {
 //Technologies:
 
 // Webcodecs -> for video and audio encoding and decoding
-//WebTransport -> for transmitting the video and audio over a network
-// 
+// WebTransport -> for transmitting the video and audio over a network
