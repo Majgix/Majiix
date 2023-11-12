2756dbfc5603a1eec2f6eafe6a9a1ee5e77e79ec
Martin Andrej Johan Grönlund <martingronlund@live.se>
2023-11-12 22:04:19 +0100

    run cargo fmt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,31 +1,29 @@
 use std::net::SocketAddr;
 
-use axum::{Router, Server, response, Json, routing::get};
+use axum::{response, routing::get, Json, Router, Server};
 
 mod env;
 
 async fn start_stream() -> response::Json<&'static str> {
-   Json("Yay!! WebTransport Connection established!")
+    Json("Yay!! WebTransport Connection established!")
 }
 
 #[tokio::main]
 async fn main() {
     let env = env::load();
 
-    let app = Router::new()
-        .route("/start-stream", get(start_stream));
+    let app = Router::new().route("/start-stream", get(start_stream));
 
-    let addr = SocketAddr::from(([127, 0 , 0, 1], env.port));
+    let addr = SocketAddr::from(([127, 0, 0, 1], env.port));
     println!("server started at addr: http://{:?}", addr);
 
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .expect("Failed to start server");   
+        .expect("Failed to start server");
 }
 
-
-//maajix is a web-based video streaming service. 
+//maajix is a web-based video streaming service.
 //Technologies:
 
 // Webcodecs -> for video and audio encoding and decoding
