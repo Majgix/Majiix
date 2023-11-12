a5e245e1b65a94217421a8425e71b3257768c802
Martin Andrej Johan Grönlund <martingronlund@live.se>
2023-11-12 20:42:16 +0100

    control app title from env

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -18,7 +18,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>{import.meta.env.VITE_APP_TITLE}</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
