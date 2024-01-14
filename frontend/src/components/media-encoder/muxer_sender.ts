a065823a486252c6c50f94c39f4a43759d578044
kelvinkirima014 <kelvinkirima2@gmail.com>
2024-01-14 11:35:00 +0300

    hardcode url params in muxer sender

--- frontend/src/components/media-encoder/muxer_sender.ts
+++ frontend/src/components/media-encoder/muxer_sender.ts
@@ -1,44 +1,20 @@
-import "./utils";
-import { State, sendMessageToMain } from './utils';
+import { State } from './utils';
 
-const WORKER_PREFIX = "[MUXER_SENDER]";
-
-let chunks_delivered = 0;
 let workerState = State.Created;
 
 // Default values
-let maxAgeChunks = 120;
-
-type maxFlightRequests = {
-    audio: any,
-    video: any,
-}
-
-const maxFlightRequests: maxFlightRequests = {
-  'audio': 4,
-  'video': 2,
-}
-
-interface inFlightRequest {
-}
-
-interface InFlightRequestsType {
-  audio: Record<string, InFlight>;
-  video: Record<string, InFlight>;
+const maxFlightRequests = {
+  audio: 4,
+  video: 2,
 };
 
+const inFlightRequests = {
+  audio: {},
+  video: {},
+};
 
-const inFlightRequests: InFlightRequestsType = {
-  'audio': {},
-  'video': {},
-}
-
-interface InFlight {
-  id: number,
-}
-
-let urlHostPort = "";
-let urlPath = "";
+let urlHostPort = "127.0.0.1";
+let urlPath = "4443";
 
 // Inflight req abort signal
 const abortController = new AbortController();
@@ -48,35 +24,28 @@ let wTransport: WebTransport | null = null;
 
 // Packager efficiency
 let efficiencyData = {
-    audio: {
-        totalPackagerBytesSent: 0,
-        totalPayloadBytesSent: 0,
-    },
-    video: {
-        totalPackagerBytesSent: 0,
-        totalPayloadBytesSent: 0,
-    }
+  audio: {
+    totalPackagerBytesSent: 0,
+    totalPayloadBytesSent: 0,
+  },
+  video: {
+    totalPackagerBytesSent: 0,
+    totalPayloadBytesSent: 0,
+  },
+};
+
+interface ChunkData {
+  mediaType: string;
+  chunk: any;
 }
 
-interface chunkData {
-    mediaType: string,
-    firstFrameclkms: number,
-    compesatedTs: number,
-    estimatedDuration: number,
-    seqId: number,
-    maxAgeChunks: number,
-    chunk: any,
-    metadata: EncodedAudioChunkMetadata | EncodedVideoChunkMetadata | null, 
-}
-
-self.addEventListener('message', async function(event) {
+self.addEventListener('message', async function (event) {
   if (workerState === State.Created) {
     workerState = State.Instatiated;
-
   }
-  
+
   if (workerState === State.Stopped) {
-    sendMessageToMain(WORKER_PREFIX, "info", "Muxer-send is Stopped");
+    console.log("muxer-sender worker stopped");
     return;
   }
 
@@ -84,7 +53,7 @@ self.addEventListener('message', async function(event) {
 
   switch (type) {
     case "stop":
-      try{
+      try {
         abortController.abort();
         await Promise.all(getAllInflightRequetsArray());
 
@@ -95,45 +64,16 @@ self.addEventListener('message', async function(event) {
       } catch (err) {
         console.error(err);
       }
-    return;
+      return;
     case "muxersenderini":
       if (workerState !== State.Instatiated) {
-        sendMessageToMain(WORKER_PREFIX, "error", "received init message in wrong state: " + workerState);
         return;
       }
 
-      let type = event.data.muxerSenderConfig;
-      switch (type) {
+      await createWebTransportSession(`https://${urlHostPort}:${urlPath}`);
 
-      
-        case "maxInflightAudioRequests":
-          maxFlightRequests['audio'] = event.data.muxerSenderConfig.maxInflightAudioRequests;
-          return;
-        case "maxInflightVideoRequests":
-          maxFlightRequests['video'] = event.data.muxerSenderConfig.maxInflightVideoRequests;
-          return;
-        case "urlHostPort":
-          urlHostPort = event.data.muxerSenderConfig.urlHostPort;
-          return;
-        case "urlPath":
-          urlPath = event.data.muxerSenderConfig.urlPath;
-          return;
-        case "maxAgeChunks":
-
-          maxAgeChunks = event.data.muxerSenderConfig.maxAgeChunks;
-          return;
-        
-        await createWebTransportSession(urlHostPort + "/" + urlPath);
-        sendMessageToMain(WORKER_PREFIX, "info", "Initialized");
-
-        workerState = State.Running;
-        return;
-      }
-  }
-
-  if ((type != "videochunk") && (type != "audiochunk")) {
-    sendMessageToMain(WORKER_PREFIX, "error", "Invalid message received");
-    return;
+      workerState = State.Running;
+      return;
   }
 
   let mediaType = "unknown";
@@ -144,131 +84,62 @@ self.addEventListener('message', async function(event) {
   }
 
   if (workerState !== State.Running) {
-    sendMessageToMain(WORKER_PREFIX, "dropped", {
-      clkms: Date.now(),
-      seqId: event.data.seqId,
-      mediaType: mediaType,inFlightRequests,
-      ts: event.data.chunk.timestamp,
-      msg: "Dropped chunk because transport is not open yet"
-    });
     return;
   }
 
-  const chunkData = {
+  const chunkData: ChunkData = {
     mediaType: mediaType,
-    firstFrameclkms: event.data.firstFrameclkms,
-    compesatedTs: event.data.compesatedTs,
-    estimatedDuration: event.data.estimatedDuration,
-    seqId: event.data.seqId,
-    maxAgeChunks: maxAgeChunks,
     chunk: event.data.chunk,
-    metadata: event.data.metadata
   };
-  if (mediaType === "audio") {
-    sendChunkToTransport(chunkData, inFlightRequests['audio'], maxFlightRequests['audio']);
-  } else {
-      sendChunkToTransport(chunkData, inFlightRequests['video'], maxFlightRequests['video']);
-    }
-  inFlightRequests
+
+  if (mediaType === "audio" || mediaType === "video") {
+    sendChunkToTransport(chunkData);
+  }
+
   // Report stats
   self.postMessage({
     type: "sendstats",
     clkms: Date.now(),
-    inFlightAudioReqNum: getInflightRequestsLength(inFlightRequests['audio']),
-    inFlightVideoReqNum: getInflightRequestsLength(inFlightRequests['video']),
+    inFlightAudioReqNum: getInflightRequestsLength(inFlightRequests.audio),
+    inFlightVideoReqNum: getInflightRequestsLength(inFlightRequests.video),
     efficiencyData: efficiencyData
   });
 
   return;
 });
 
-function sendChunkToTransport(
-  chunkData: chunkData,
-  inFlightRequests: inFlightRequest,
-  maxFlightRequests: maxFlightRequests,
-) {
+function sendChunkToTransport(chunkData: ChunkData) {
   if (chunkData == null) {
-    return;
-  } 
-  if (getInflightRequestsLength(inFlightRequests) >= maxFlightRequests.audio) {
-    sendMessageToMain(WORKER_PREFIX, "dropped", {
-      clkms: Date.now(),
-      seqId: chunkData.seqId,
-      mediaType: chunkData.mediaType,
-      ts: chunkData.compesatedTs,
-      msg: "Dropped chunk, too many inFlightRequests",
-    });
     return;
   }
   return createRequests(chunkData);
 }
 
-function getInflightRequestsLength(inFlightRequestsType: inFlightRequest) {
+function getInflightRequestsLength(inFlightRequestsType: any) {
   return Object.keys(inFlightRequestsType).length;
 }
 
 function getAllInflightRequetsArray() {
-   const arrAudio = Object.values(inFlightRequests['audio']);
-   const arrVideo = Object.values(inFlightRequests['video']);
+  const arrAudio = Object.values(inFlightRequests.audio);
+  const arrVideo = Object.values(inFlightRequests.video);
 
-   return arrAudio.concat(arrVideo)
+  return arrAudio.concat(arrVideo);
 }
 
-function createRequests(chunkData: chunkData) {
-  if (chunkData !== null){
+function createRequests(chunkData: ChunkData) {
+  if (chunkData !== null) {
     const ret = [];
     const mediaType = chunkData.mediaType;
-    const seqId = chunkData.seqId;
-    const maxAgeChunks = chunkData.maxAgeChunks;
     const chunk = chunkData.chunk;
-    const metadata = chunkData.metadata;
-    const firstFrameClkms = chunkData.firstFrameclkms;
-    const compesatedTs = chunkData.compesatedTs;
-    const estimatedDuration = chunkData.estimatedDuration;
-    
-    
-    if (metadata != undefined) {
-        let pIni = null;
-        pIni = createWebTransportRequestPromise(
-          firstFrameClkms, 
-          mediaType, 
-          "init", 
-          compesatedTs, 
-          estimatedDuration, 
-          -1, 
-          Number.MAX_SAFE_INTEGER, 
-          metadata
-        );
-        ret.push(pIni);
-    }
 
     const chunkDataBuffer = new Uint8Array(chunk.byteLength);
     chunk.copyTo(chunkDataBuffer);
 
     let pChunk = null;
-    pChunk = createWebTransportRequestPromise(firstFrameClkms, mediaType, chunk.type, compesatedTs, estimatedDuration, seqId, maxAgeChunks, metadata);
+    pChunk = createWebTransportRequestPromise(mediaType, chunk.type);
     ret.push(pChunk);
 
     ret;
-  } 
-}
-
-
-function addToInFlight(mediaType: string, p: InFlight) {
-  if (mediaType === 'audio' || mediaType === 'video') {
-    if (p.id in inFlightRequests[mediaType]) {
-      sendMessageToMain(WORKER_PREFIX, "error", "id already exists in inflight, this should never happen");
-    } else {
-      inFlightRequests[mediaType][p.id] = p;
-    }
-  }
-}
-
-function removeFromInflight(mediaType: string, id: number) {
-  if (mediaType === 'audio' || mediaType === 'video') {
-    if (id in inFlightRequests[mediaType]) {
-        delete inFlightRequests[mediaType][id];
-    }
   }
 }
 
@@ -279,56 +150,25 @@ async function createWebTransportSession(url: string) {
   wTransport = new WebTransport(url);
   await wTransport.ready;
 
-  wTransport.closed
-        .then(() => {
-            sendMessageToMain(WORKER_PREFIX, "info", "WT closed transport session");
-        })
-        .catch(error => {
-            sendMessageToMain(WORKER_PREFIX, "error", "WT error, closed transport. Err: " + error);
-  });
+  wTransport.closed;
 }
 
 async function createWebTransportRequestPromise(
-  firstFrameclkms: number,
   mediaType: string,
   chunkType: string,
-  compesatedTs: number,
-  estimatedDuration: number,
-  seqId: number,
-  maxAgeChunks: number,
-  metadata?: any,
 ) {
- if (wTransport === null) {
-    sendMessageToMain(
-      WORKER_PREFIX,
-      "dropped",
-      {
-        clkms: Date.now(),
-        ts: compesatedTs,
-        msg: "Dropped" + mediaType + "because of server error",
-      }
-    );
-    sendMessageToMain(
-      WORKER_PREFIX,
-      "error",
-      "request not sent because transport is not open",
-    );
+  if (wTransport === null) {
     return;
   }
-  
+
   try {
     const uniStream = await wTransport.createUnidirectionalStream();
     const uniWriter = uniStream.getWriter();
     await uniWriter.ready;
 
     const chunkObject = {
-      maxAgeChunks,
       mediaType,
       chunkType,
-      compesatedTs,
-      estimatedDuration,
-      seqId,
-      metadata
     };
 
     const objectBytes = new TextEncoder().encode(JSON.stringify(chunkObject));
@@ -336,12 +176,12 @@ async function createWebTransportRequestPromise(
     uniWriter.write(objectBytes);
 
     const p = uniWriter.close();
-   
-   // addToInFlight(mediaType, p);
-  } catch(err: any) {
-    sendMessageToMain(WORKER_PREFIX, "error", "request: " + mediaType + "-" + seqId + ". Err: " + err.message);
+
+  } catch (err: any) {
+    console.error(err);
   }
   return null;
 }
 
 export {};
+
